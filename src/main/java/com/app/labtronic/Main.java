6e226d8c929e77d25cea2e3287a6e95d94de1073
RobertR-01 <74870787+RobertR-01@users.noreply.github.com>
2023-01-22 21:39:48 +0100

    Initial commit
    Basic UI for the main app window.

---
+++ src/main/java/com/app/labtronic/Main.java
@@ -0,0 +1,9 @@
+package com.app.labtronic;
+
+import com.app.labtronic.ui.MainApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.app.labtronic {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.bootstrapicons;
+    requires org.kordamp.ikonli.boxicons;
+
+    opens com.app.labtronic to javafx.fxml;
+    exports com.app.labtronic;
+    exports com.app.labtronic.ui;
+    opens com.app.labtronic.ui to javafx.fxml;
+}
